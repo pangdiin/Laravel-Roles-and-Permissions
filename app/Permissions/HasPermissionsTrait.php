ac5f66249f6a9a483f989487c32848c13f54afe2
Guiller <guillermo.halcyondigital@gmail.com>
2017-03-13 11:59:54 +0800

    creating permissiontrait functions

--- app/Permissions/HasPermissionsTrait.php
+++ app/Permissions/HasPermissionsTrait.php
@@ -8,6 +8,26 @@ use App\Permission;
 
 trait HasPermissionsTrait
 {
+	public function hasPermissionTo($permission)
+	{
+		return $this->hasPermission($permission)
+	}
+
+	public function hasRole(...$roles)
+	{
+		foreach ($roles as $role) {
+			if ($this->roles->contains('name', $role)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function hasPermission($permission)
+	{
+		return (bool) $this->permissions->where('name', $permission->name)->count();
+	}
+
 	public function roles()
 	{
 		return $this->belongsToMany(Role::class, 'users_roles');
